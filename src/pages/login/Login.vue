c2cc3308fada34ac3bde9f2f93e6810e25ce0228
EK <qxjj7ay@163.com>
2019-11-13 17:11:55 +0800

    electricity-start

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login">{{info}}</div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data () {
+            return {
+              info: "hello world"
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .login
+    font-size .40rem
+    color $bgColor
+</style>
