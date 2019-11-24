522c559666c9bceb4e14ded7d91ce1b3c47eca56
EK <qxjj7ay@163.com>
2019-11-24 22:47:44 +0800

    1124(start)

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,11 +1,52 @@
 <template>
-    <div class="login">{{info}}
-        <van-button type="warning">警告按钮</van-button>
-        <van-button type="default">默认按钮</van-button>
-        <van-button type="primary">主要按钮</van-button>
-        <van-button type="info">信息按钮</van-button>
-        <van-button type="warning">警告按钮</van-button>
-        <van-button type="danger">危险按钮</van-button>
+    <div class="login">
+      <div class="login-box">
+        <van-icon class="closeIcon" name="close" />
+        <van-tabs v-model="active">
+          <van-tab title="登录">
+            <van-cell-group>
+              <van-field
+                v-model="phone"
+                required
+                clearable
+                label="手机号"
+                placeholder="请输入手机号"
+              />
+              <van-field
+                v-model="sms"
+                required
+                center
+                clearable
+                label="短信验证码"
+                placeholder="验证码"
+              >
+                <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+              </van-field>
+            </van-cell-group>
+            <van-button class="btn-login" type="info" size="large">登录</van-button>
+          </van-tab>
+
+          <van-tab title="注册">
+            <van-cell-group>
+              <van-field
+                v-model="phone"
+                required
+                clearable
+                label="手机号"
+                placeholder="请输入手机号"
+              />
+              <van-field
+                v-model="sms"
+                required
+                clearable
+                label="密码"
+                placeholder="请输入密码（不少于6位）"
+              />
+            </van-cell-group>
+            <van-button class="btn-login" type="info" size="large">注册</van-button>
+          </van-tab>
+        </van-tabs>
+      </div>
     </div>
 </template>
 
@@ -14,7 +55,9 @@
         name: "Login",
         data () {
             return {
-              info: "hello world"
+              active: 2,
+              phone: "",
+              sms: ""
             }
         }
     }
@@ -23,6 +66,27 @@
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .login
-    font-size .40rem
-    color $bgColor
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 0
+    background url("../../assets/img/login/back2.jpg") no-repeat
+    background-size cover
+    background-attachment fixed
+    display flex
+    justify-content center
+    align-items center
+    .login-box
+      position relative
+      width 90%
+      background $bgColor
+      padding .70rem
+      .closeIcon
+        position absolute
+        top .14rem
+        right .30rem
+        font-size .40rem
+      .btn-login
+        margin-top .40rem
 </style>
