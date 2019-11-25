40030ac4d83dde299cb97238c394aa0852dd7811
EK <qxjj7ay@163.com>
2019-11-25 18:20:15 +0800

    112501

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -4,7 +4,8 @@
         <van-icon class="closeIcon" name="close" />
         <van-tabs v-model="active">
           <van-tab title="登录">
-            <van-cell-group>
+            <!--手机验证码登陆-->
+            <van-cell-group v-show="!isShowSMSLogin">
               <van-field
                 v-model="phone"
                 required
@@ -21,6 +22,37 @@
                 placeholder="验证码"
               >
                 <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+              </van-field>
+            </van-cell-group>
+            <!--账号密码登陆-->
+            <van-cell-group v-show="isShowSMSLogin">
+              <van-field
+                v-model="phone"
+                required
+                clearable
+                label="手机号"
+                placeholder="请输入手机号"
+              />
+              <van-field
+                v-model="sms"
+                required
+                center
+                clearable
+                label="密码"
+                placeholder="请输入密码"
+              >
+              </van-field>
+              <van-field v-model="imgCaptcha"
+                center
+                clearable
+                class="verificationImageBox"
+                maxlength="4"
+                placeholder="请输入验证码">
+                <img src="http://demo.itlike.com/web/xlmc/api/captcha"
+                     alt="captcha"
+                     @click.prevent="getCaptcha"
+                     ref="imgCaptcha"
+                     slot="button">
               </van-field>
             </van-cell-group>
             <van-button class="btn-login" type="info" size="large">登录</van-button>
@@ -56,10 +88,20 @@
         data () {
             return {
               active: 2,
-              phone: "",
-              sms: ""
+              phone: "",       //手机号
+              sms: "",         //短信验证码
+              isShowSMSLogin: true ,     //是否短信登陆
+              imgCaptcha: ""   //图片验证码
             }
-        }
+        },
+        methods: {
+          // 3.账号密码登录切换图片验证码
+          getCaptcha () {
+            // 获取验证码的标签
+            let captchaEle = this.$refs.imgCaptcha;
+            this.$set(captchaEle, 'src', 'http://demo.itlike.com/web/xlmc/api/captcha?time=' + new Date());
+          },
+        },
     }
 </script>
 
@@ -71,7 +113,7 @@
     left 0
     right 0
     bottom 0
-    background url("../../assets/img/login/back2.jpg") no-repeat
+    background url("../../assets/img/login/lobg.png") no-repeat
     background-size cover
     background-attachment fixed
     display flex
@@ -81,12 +123,17 @@
       position relative
       width 90%
       background $bgColor
+      opacity 0.9
       padding .70rem
       .closeIcon
         position absolute
         top .14rem
         right .30rem
         font-size .40rem
+      .verificationImageBox
+        padding: 0 16px;
+        img 
+          height .88rem
       .btn-login
         margin-top .40rem
 </style>
